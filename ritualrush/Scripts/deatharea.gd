3f8d954af7efc733773acb2444eba64ebf1e8d37
Hindruck <leteusen@stud.mediadesign.de>
2025-01-14 19:15:32 +0100

    Adding Death as a Scene to modify later and implementing movement animations
    I may move to an animation tree soon, but currently still testing options

--- ritualrush/Scripts/deatharea.gd
+++ ritualrush/Scripts/deatharea.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
+@onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	Engine.time_scale = 0.5
+	
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
--- ritualrush/Scripts/player.gd
+++ ritualrush/Scripts/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var isPlayingAnimation: bool = false 
+
+@onready var animated_sprite_player: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +15,42 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Flip to face the direction of the movement
+	if direction > 0:
+		animated_sprite_player.flip_h = false
+	elif direction < 0:
+		animated_sprite_player.flip_h = true
+	
+	# Only updating the movement Animations when no other animation is played
+	if !isPlayingAnimation:
+		# Movement Animations
+		# Idle when not moving and Run when moving in any direction
+		if direction == 0:
+			animated_sprite_player.play("idle")
+		else:
+			animated_sprite_player.play("run")	
+		
+		# Air Animations
+		# Jump and Fall depending on the velocity
+		if velocity.y < 0 and !is_on_floor():
+			animated_sprite_player.play("jump")
+		elif velocity.y > 0 and !is_on_floor():
+			animated_sprite_player.play("fall")
+	
 	move_and_slide()
+
+
+func _process(delta: float) -> void:
+	pass
+		
+	
